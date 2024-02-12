df0669d8773b693c64a784eb7e6c8e1b6daf1782
Looopy8 <lionelchew8@gmail.com>
2024-02-12 23:58:00 +0800

    First

---
+++ ABC/Assets/Scripts/BirdScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    float height = 4f;
+    Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newY = Mathf.Sin(Time.time * 2) * height + pos.y;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
