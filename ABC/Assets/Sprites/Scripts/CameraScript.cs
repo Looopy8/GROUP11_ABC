81bf86b1f5caac6ce4142fd1c4d17b353fcaed4b
reakito <yeminkhanttt@gmail.com>
2024-02-22 21:44:45 +0800

    MinKhantAgain
    Created Pause Scene
    Touched Up on 2nd & 3rd scene

---
+++ ABC/Assets/Sprites/Scripts/CameraScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject Pause;
+    public bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x + 6, 0, -10);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            isPaused = !isPaused;
+            Time.timeScale = isPaused ? 0 : 1;
+            if(isPaused == true)
+            {
+                Pause.SetActive(true);
+            }
+            else
+            {
+                Pause.SetActive(false);
+            }
+
+        }
+        
+
+    }
+    
+}
---
+++ ABC/Assets/Sprites/Scripts/PigScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigScript : MonoBehaviour
+{
+    float height = 3f;
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newX = Mathf.Sin(Time.time * 1) * height + pos.x;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Turn")
+        {
+            transform.Rotate(Vector3.up * 180);
+        }
+        
+    }
+}
